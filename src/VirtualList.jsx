705f3e918d3f7bee43d911435fb3c7c2331cc50f
Corey Burrows <corey.burrows@centro.net>
2016-05-09 14:21:54 -0500

    Implements downward scroll without using a native scrollbar.

--- src/VirtualList.jsx
+++ src/VirtualList.jsx
@@ -12,13 +12,7 @@ const VirtualList = React.createClass({
   },
 
   getInitialState() {
-    const {items, windowSize, estRowHeight} = this.props;
-
-    return {
-      winStart: 0,
-      paddingTop: 0,
-      paddingBottom: (items.length - windowSize) * estRowHeight
-    };
+    return {winStart: 0, top: 0};
   },
 
   componentWillMount() {
@@ -26,112 +20,68 @@ const VirtualList = React.createClass({
     this._itemView = React.Children.only(this.props.children);
   },
 
-  adjustPaddingTopForUpwardScroll(n) {
-    const {content: {childNodes}} = this.refs;
-    const {estRowHeight} = this.props;
-    let {paddingTop} = this.state;
-
-    for (let i = 0; i < n; i++) {
-      paddingTop -= childNodes[i].offsetHeight;
-    }
-
-    this.setState({paddingTop});
-  },
-
-  handleDownwardScroll() {
+  scrollDownward(delta) {
     const {node, content} = this.refs;
     const {items, windowSize, estRowHeight} = this.props;
-    const scrollTop = node.scrollTop;
     const itemNodes = content.childNodes;
-    let {winStart, paddingTop} = this.state;
-    let paddingTopAdjust = 0;
+    const maxWinStart = items.length - windowSize;
+    let {winStart, top} = this.state;
 
-    for (let i = 0; i < itemNodes.length; i++) {
-      if (itemNodes[i].offsetTop + itemNodes[i].offsetHeight < scrollTop) {
-        winStart++;
-        paddingTopAdjust += estRowHeight + (itemNodes[i].offsetHeight - estRowHeight);
-      }
-      else {
-        break;
-      }
-    }
-
-    this.setState({
-      winStart,
-      paddingTop: paddingTop + paddingTopAdjust,
-      paddingBottom: (items.length - windowSize - winStart) * estRowHeight
-    });
-  },
-
-  handleUpwardScroll() {
-    const {node, content} = this.refs;
-    const {items, estRowHeight, windowSize} = this.props;
-    const scrollTop = node.scrollTop;
-    const itemNodes = content.childNodes;
-    let {winStart} = this.state;
-    let n = 0;
-
-    for (let i = itemNodes.length - 1; i >= 0; i--) {
-      if (winStart > 0 && itemNodes[i].offsetTop > (scrollTop + node.offsetHeight)) {
-        winStart--;
-        n++;
-      }
-      else {
-        break;
-      }
-    }
-
-    this.setState({
-      winStart,
-      paddingBottom: (items.length - windowSize - winStart) * estRowHeight
-    }, () => { this.adjustPaddingTopForUpwardScroll(n) });
-  },
-
-  handleLongScroll() {
-    const {node: {scrollTop}} = this.refs;
-    const {items, windowSize, estRowHeight} = this.props;
-    const winStart = Math.floor(scrollTop / estRowHeight);
-    const paddingTop = winStart * estRowHeight;
-    const paddingBottom = (items.length - windowSize - winStart) * estRowHeight;
-
-    this.setState({winStart, paddingTop, paddingBottom});
-  },
-
-  onScroll(e) {
-    const {node, node: {scrollTop}, content} = this.refs;
-    const {estRowHeight} = this.props;
-    const {winStart, paddingTop, paddingBottom} = this.state;
-    const scrollDelta = Math.abs(scrollTop - this._scrollTop);
-
-    if (scrollDelta > (content.offsetHeight - paddingTop - paddingBottom)) {
-      this.handleLongScroll();
-    }
-    else if (scrollTop > this._scrollTop) {
-      this.handleDownwardScroll();
+    if (winStart === maxWinStart) {
+      top = Math.min(content.offsetHeight - node.offsetHeight, top + delta);
     }
     else {
-      this.handleUpwardScroll();
+      top += delta;
     }
 
-    this._scrollTop = scrollTop;
+    for (let i = 0; i < itemNodes.length; i++) {
+      if (winStart < maxWinStart && itemNodes[i].offsetTop + itemNodes[i].offsetHeight < top) {
+        winStart++;
+        top -= itemNodes[i].offsetHeight;
+      }
+      else {
+        break;
+      }
+    }
+
+    this.setState({winStart, top});
+  },
+
+  scrollUpward() {
+  },
+
+  scroll(delta) {
+    if (delta > 0) {
+      this.scrollDownward(delta);
+    }
+    else if (delta < 0) {
+      this.scrollUpward(-delta);
+    }
+  },
+
+  onWheel(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.deltaY !== 0) { this.scroll(e.deltaY); }
   },
 
   render() {
     const {items, estRowHeight, windowSize} = this.props;
-    const {winStart, paddingTop, paddingBottom} = this.state;
+    const {winStart, top} = this.state;
     const style = {
       position: 'absolute',
       top: 0,
       right: 0,
       bottom: 0,
-      left: 0,
-      overflowX: 'hidden',
-      overflowY: 'auto'
+      left: 0
     };
-    const contentStyle = {paddingTop, paddingBottom};
+    const contentStyle = {
+      position: 'absolute',
+      top: -top
+    };
 
     return (
-      <div ref="node" className="VirtualList" style={style} onScroll={this.onScroll}>
+      <div ref="node" className="VirtualList" style={style} onWheel={this.onWheel}>
         <div ref="content" className="VirtualList-content" style={contentStyle}>
           {
             items.slice(winStart, winStart + windowSize).map(item =>
